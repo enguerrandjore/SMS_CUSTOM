11523f7207209774c81c6e19b2857c391080916c
enguerrandJore <enguerrand.jore@atos.net>
2019-09-02 22:23:39 +0200

    action et non info pour le phone numbre de l'expediteur

--- core/class/sms.class.php
+++ core/class/sms.class.php
@@ -166,7 +166,7 @@ class sms extends eqLogic {
 			$numExped->setIsVisible(0);
 			$numExped->setName(__('expediteur_reel', __FILE__));
 		}
-		$numExped->setType('info');
+		$numExped->setType('action');
 		$numExped->setSubType('string');
 		$numExped->setEqLogic_id($this->getId());
 		$numExped->save();
